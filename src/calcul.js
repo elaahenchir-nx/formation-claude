c34652ca4153bcb9c15d5144975f9c75f5064451
elaa.henchir <elaa.henchir@neoxam.com>
2026-08-11 09:46:31 +0100

    undo changes

--- src/calcul.js
+++ src/calcul.js
@@ -1,7 +1,14 @@
+// Version baseline (saine) - a committer en premier sur main
+
+const cacheRemises = new Map();
 
 function calculerRemiseFidelite(client) {
+  if (cacheRemises.has(client.id)) {
+    return cacheRemises.get(client.id);
+  }
+
   let remise = 0;
-  if (client.anneesAnciennete >= 6) {
+  if (client.anneesAnciennete >= 5) {
     remise = 0.15;
   } else if (client.anneesAnciennete >= 2) {
     remise = 0.10;
@@ -9,6 +16,7 @@ function calculerRemiseFidelite(client) {
     remise = 0.05;
   }
 
+  cacheRemises.set(client.id, remise);
   return remise;
 }
 
